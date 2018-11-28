80738e92fa651fbb3bc271560dff2f14836aa1ce
okamoto <wednesday.developer@gmail.com>
2018-11-29 08:17:04 +0900

    storybookを追加

---
+++ src/stories/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    @click="handleClick"
+    :disabled="disabled||false"
+    :class="[
+    'button',
+    'button-' + (disabled ? 'disabled' : kind),
+    size  ? 'button-'+size : 'button-normal'
+    ]"
+  >
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String
+    },
+    kind: {
+      type: String
+    },
+    size: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onclick (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
